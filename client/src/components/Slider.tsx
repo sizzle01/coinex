0eaa5b45d133d0ec2eb2ea1e408fa195dc72f572
sizzle01 <talktoaliyu@gmail.com>
2022-09-26 10:54:37 -0400

    I added styles to onboarding page descriptions

--- client/src/components/Slider.tsx
+++ client/src/components/Slider.tsx
@@ -1,35 +1,47 @@
 import React from "react";
 
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar,Autoplay } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
 
-import "swiper/css";
-import Image from "next/image";
+
+import Link from "next/link";
+
 interface SliderProps {}
 
 const items = [
   {
-    image: <img src="/slider2.png" alt="atm" />,
+    image: <img src="/slider1.png" alt="atm" />,
     name: "Manage your digital asset",
     description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium",
+      "Our App helps to you store, organize, find, retrieve your digital asset",
   },
   {
-    image: <img src="/slider2.png" alt="atm" />,
+    image: <img src="/slider3.png" alt="atm" />,
     name: "Grow your Funds",
     description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium",
+      "If you are wondering how to earn more money with your cryptocurrency , look no further! this is the best platform for you",
   },
   {
     image: <img src="/slider2.png" alt="atm" />,
     name: "Pay anywhere, anytime",
     description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium",
+      "We offer incredibly fast payment solutions for you and your business. make payments at ease anyhwere anytime",
   },
 ];
 const Slider: React.FC<SliderProps> = () => {
   return (
     <div className="h-[100vh] w-full">
       <Swiper
+        navigation
+        modules={[Navigation, Pagination, Scrollbar, Autoplay]}
+        autoplay={{delay:2000}}
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
         spaceBetween={0}
         slidesPerView={1}
         onSwiper={(swiper) => console.log(swiper)}
@@ -38,24 +50,32 @@ const Slider: React.FC<SliderProps> = () => {
         {items.map((item, index) => (
           <div
             key={index}
-            className="h-[100vh] flex flex-col w-full  justify-center items-center  "
+            className="h-[100vh] flex flex-col w-full  justify-center items-center"
           >
             <SwiperSlide className="h-screen flex flex-col w-full  justify-center items-center bg-gradient-to-r from-purple-600 via-pink-600 to-blue-500 ">
               <div className=" w-full flex flex-col w-full  justify-center items-center">
-                <div className="h-[500px] w-[500px]">{item.image}</div>
+                <div className="h-[400px] w-[500px]">{item.image}</div>
 
-                <div className="text-center text-xl text-white">
+                <div className="mt-[-20px] font-semibold text-center text-xl text-white">
                   {item.name}
                 </div>
                 <div className=" text-base text-center text-white px-5">
                   {item.description}
                 </div>
+               <p className={`${index === 2 ? 'hidden' : ''} text-white text-xl mt-10`}>
+                <Link href="welcome"> skip</Link>
+              
+                </p>
+                <Link href="welcome">
                 <button
+  
                   type="button"
-                  className="text-white w-[50%] md:w-[20%] mt-2 border-[1px] p-2 border-white hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+                  className={`${index === 2 ? '' : 'hidden'} mt-10 text-white w-[50%] md:w-[20%] mt-2 border-[1px] p-2 border-white hover:bg-[#3d4f7c] rounded-full cursor-pointer`}
                 >
-                  Continue
+                 Continue
+                  
                 </button>
+                </Link>
               </div>
             </SwiperSlide>
           </div>
