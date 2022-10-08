93f7ce6f59ebb3f03435ce5526843e44671932f3
sizzle01 <talktoaliyu@gmail.com>
2022-10-08 09:51:33 -0400

    i completed the footer section

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const Footer = () => (
   <section className={`${styles.flexCenter}  ${styles.paddingY} flex-col  bg-primary`}>
-    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-[80%]`}>
       <div className="flex-[1] flex flex-col justify-start mr-10">
         <Image
           src={Logo}
@@ -14,7 +14,7 @@ const Footer = () => (
           width={80}
           className="w-[266px] h-[72.14px] object-contain"
         />
-        <p className={`${styles.paragraph} mt-4 max-w-[312px] md:ml-[35%]`}>
+        <p className={`${styles.paragraph} mt-4 max-w-[312px] ml-[13%] md:ml-[33%]`}>
           A new way to make the cryptocurrency and other digital asset transactions, reliable and secure.
         </p>
       </div>
@@ -42,12 +42,12 @@ const Footer = () => (
       </div>
     </div>
 
-    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+    <div className="w-[80%] flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
         Copyright Ⓒ 2022 Crypto. All Rights Reserved.
       </p>
 
-      <div className="flex flex-row md:mt-0 mt-6 ">
+      <div className="flex flex-row md:mt-0 mt-6">
         {socialMedia.map((social, index) => (
           <Image
             key={social.id}
