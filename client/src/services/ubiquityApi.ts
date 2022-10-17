aa5fbcf29de7f0eb95c17407008243ebca8854f9
Qreamville <Oyeleyebalikis@yahoo.com>
2022-10-16 22:35:49 -1200

    Update ubiquityApi.ts

--- client/src/services/ubiquityApi.ts
+++ client/src/services/ubiquityApi.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const instance = axios.create({
   baseURL: "https://svc.blockdaemon.com/",
-  headers: { Authorization: `Bearer ${process.env.NEXT_PUBLIC_UBIQUITY_API}` },
+  headers: {
+    Authorization: `Bearer 9lNOzUvOUrDIKlhmBWzVZRmV7gudlgIeYiLgp9fOPX79Uq7s`,
+  },
 });
 // ethereum/goerli/account/0xF0ccc8B440Bf013a37ef722530B1e4727a785CfA/txs
